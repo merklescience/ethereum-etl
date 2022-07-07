30247951aefbcb8fbfea510873ff3532c6dbc3a7
saurabhdaga-merkle <saurabh@merklescience.com>
2022-07-07 15:40:16 +0530

    start warm

--- start-warm.sh
+++ start-warm.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-source env/bin/activate
+source /root/ethereum-etl/env/bin/activate
 python ethereumetl.py stream --provider-uri $ETHEREUM_PROVIDER_URI -w 5 -B 20 -b 5 --period-seconds 5 -l last_synced_block_warm.txt --lag 18 -e transaction,token_transfer,trace -o kafka -t eth.warm
