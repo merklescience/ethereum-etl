81d3804caae00b4bfcc6f6142ad154184d482aba
Akshay Gupta <akshay@merklescience.com>
2023-08-29 12:11:20 +0530

    start-hot.sh update

--- start-hot.sh
+++ start-hot.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 source /root/ethereum-etl/env/bin/activate
-python /root/ethereum-etl/ethereumetl.py stream --provider-uri $ETHEREUM_PROVIDER_URI -w 35 -B 20 -b 5 --period-seconds 1 -l /root/ethereum-etl/last_synced_block_hot.txt -e transaction,token_transfer,trace -o kafka -t eth.hot
+python /root/ethereum-etl/ethereumetl.py stream --provider-uri $ETHEREUM_PROVIDER_URI -w 5 -B 3 -b 3 --period-seconds 2 -l /root/ethereum-etl/last_synced_block_hot.txt --lag 0 -e transaction,token_transfer,trace -o kafka -t producer-ethereum - ts hot
