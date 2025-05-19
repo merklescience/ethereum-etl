4718badf0d454d87ed2a468279f104a814a3ebc8
akshay-ghy <akshay@merklescience.com>
2025-05-19 16:17:17 +0530

    revert batch size and compression

--- blockchainetl/jobs/exporters/ms_kafka_exporter.py
+++ blockchainetl/jobs/exporters/ms_kafka_exporter.py
@@ -48,11 +48,11 @@ class KafkaItemExporter:
             "sasl.username": os.getenv("KAFKA_PRODUCER_KEY"),
             "sasl.password": os.getenv("KAFKA_PRODUCER_PASSWORD"),
             "linger.ms": 10,
-            "batch.size": 100,
+            "batch.size": 5000,
             "acks": 1,
-            "batch.num.messages": 100,
+            "batch.num.messages": 1000,
             "delivery.report.only.error": True,
-            "compression.type": "lz4"
+            "compression.type": "gzip"
         }
 
         producer = Producer(conf)
